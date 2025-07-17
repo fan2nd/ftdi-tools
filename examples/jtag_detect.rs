786e5cb545c8fee4a007c6dc6e3fe4520d50ce5a
fan <fan@fandeMac-mini.local>
2025-07-17 10:50:14 +0800

    update

--- examples/jtag_detect.rs
+++ examples/jtag_detect.rs
@@ -13,10 +13,9 @@ fn main() -> anyhow::Result<()> {
     assert!(!devices.is_empty(), "Not found Ftdi devices");
     let mpsse = FtdiMpsse::open(&devices[0].usb_device, devices[0].interface[0], 0)?;
     let mtx = Arc::new(Mutex::new(mpsse));
-    let pins = [0, 1, 2, 3, 4, 5, 6, 7];
     let mut notdi = Vec::new();
     // find tdo
-    for couple in pins.into_iter().permutations(2) {
+    for couple in (0..8).into_iter().permutations(2) {
         let tck = couple[0];
         let tms = couple[1];
         let jtag = JtagDetectTdo::new(mtx.clone(), tck, tms)?;
@@ -25,27 +24,23 @@ fn main() -> anyhow::Result<()> {
         let tdo_pins = jtag.scan()?;
         for tdo in tdo_pins.into_iter() {
             if let Pin::Lower(tdo_idx) = tdo {
-                if !pins.contains(&tdo_idx) {
-                    continue;
-                }
                 notdi.push((tck, tms, tdo_idx));
             }
         }
     }
     // find tdi
     for (tck, tms, tdo) in notdi {
-        for &tdi in pins.iter() {
+        // you can add code here to control level translation chip
+        // tdo input, other output
+        for tdi in 0..8 {
             if tdi == tck || tdi == tms || tdi == tdo {
                 continue;
             }
             let jtag = JtagDetectTdi::new(mtx.clone(), tck, tdi, tdo, tms)?;
-            // you can add code here to control level translation chip
-            // tck and tms and tdi output, tdo input
             let ids_scan1 = jtag.scan_with(true)?;
             let ids_scan0 = jtag.scan_with(false)?;
             // Scanning with 1 will give you 32 more bypasses than scanning with 0
             if ids_scan0.len() - ids_scan1.len() == 32 {
-                // use 1 scan will
                 println!("!!!!!! Pins:tck[{tck}],tdi[{tdi}],tdo[{tdo}],tms[{tms}]");
                 println!("!!!!!! Found Devices:{ids_scan1:x?}");
             }
